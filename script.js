8742aad3cf4d1d1768c436658e4d90d48279527e
Naseela-KT <75978405+Naseela-KT@users.noreply.github.com>
2023-07-07 07:21:34 +0530

    first-commit

---
+++ script.js
@@ -0,0 +1,28 @@
+function hideSide(){
+    document.getElementById("side-bar").style.visibility="hidden";
+}
+
+function showSide(){
+    document.getElementById("side-bar").style.visibility="visible";
+}
+
+
+// GOALS
+function showGoalsLeft(){
+    document.getElementById("goalsleft").style.visibility="visible";
+    document.getElementById("goalsright").style.visibility="hidden";
+}
+function hideGoalsRight(){
+    document.getElementById("goalsleft").style.visibility="hidden";
+    document.getElementById("goalsright").style.visibility="visible";
+}
+
+
+
+function showLeftButton(){
+    document.getElementById("left-button").style.visibility="visible";
+}
+
+function hideButton(){
+    document.getElementById("left-button").style.visibility="hidden";
+}
